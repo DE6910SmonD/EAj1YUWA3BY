e062998ea4cf495ba5895878b103ea2d4a6fb62b
DE6910SmonD <willow.credo0i@icloud.com>
2025-04-02 00:27:03 +0300

    Fix a problem with the file's sorting algorithm

--- B1S-5/elQGQ/g5Duv/x7sF/observability_fmrZPA_1741803920.js
+++ B1S-5/elQGQ/g5Duv/x7sF/observability_fmrZPA_1741803920.js
@@ -1,5 +1,4 @@
 // @ts-check
-const emotionPlugin = require('@emotion/eslint-plugin');
 const importPlugin = require('eslint-plugin-import');
 const jestPlugin = require('eslint-plugin-jest');
 const jsxA11yPlugin = require('eslint-plugin-jsx-a11y');
